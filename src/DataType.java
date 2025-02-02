988255cd6a179dfdc03e365ea5e4f366fedeafae
Tamas Toth <91toto02@gmail.com>
2025-02-02 12:59:41 +0100

    Add datatype class for the proyejt

--- src/DataType.java
+++ src/DataType.java
@@ -1,2 +1,59 @@
+import jdk.internal.icu.text.UnicodeSet;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class DataType {
+
+    public static void main(String[] args) {
+
+        byte number1 =100;
+        short number2 = 10000;
+        int number3 = 100000;
+        long number4 = 10000000000L;
+
+        float number5 = 3.14f;
+        double number6 = 3.14;
+
+        char letter = 'a';
+        boolean isValid = true;
+
+        Byte number11 = number1; //auto-boxing wrapper osztály segitségével
+        number11.byteValue();
+        number11.toString();
+
+        String name = "John";
+        String name2 = "john";
+        String name3 = "John";
+        String name4 = new String( "John");
+
+        boolean isSame1 = name == name2;
+        System.out.println(isSame1);
+        boolean isSame2= name == name3;
+        System.out.println(isSame2);
+        boolean isSame3 = name == name4;
+        System.out.println(isSame3);
+        boolean isSame4 = name.equals(name2); // egyenlőség
+        System.out.println(isSame4);
+        boolean isSame5 = name.equalsIgnoreCase(name2); // kis-nagy betű figyelmen kívül hagyása
+        System.out.println(isSame5);
+        boolean isSame6 = name.equals(name4);
+        System.out.println(isSame6);
+
+        List<Integer> numbers = new ArrayList<>();
+        numbers.add(1);
+        numbers.contains(1);
+        numbers.remove(0);
+        int size = numbers.size();
+        System.out.println(size);
+
+        int[] nums = {1, 2, 3, 4, 5, 6, 7,};
+        int length = nums.length;
+        System.out.println(length);
+
+        List<Integer> numbers2 = new ArrayList<>();
+        numbers.toArray();
+        Integer[] nums2 = {1, 2, 3, 4, 5, 6,};
+
+    }
 }
